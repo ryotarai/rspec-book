7bc8bdd2286c14c98f1c14102d8162a764868817
Ryota Arai <ryota.arai@gmail.com>
2013-04-09 07:58:58 +0900

    ~p.330

---
+++ messages/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messages::Application.config.secret_token = '216c8904c309ddf9c6a768a8d7f7ee35f7f00b021068ece2a82117beffa33b183101ae6326f3353394a9348c97cb9f23bcc35813e7ad47119d15ccff688d5765'
---
+++ messages/spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe MessagesController do
+  describe "POST create" do
+    it "creates a new message" do
+      Message.should_receive(:new).with("text" => "a quick brown fox")
+      post :create, message: {"text" => "a quick brown fox"}
+    end
+    it "saves the message"
+  end
+end
