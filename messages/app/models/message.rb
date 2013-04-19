a8ee8cad5307fa0864e541bb3713224fa7b679d6
Ryota Arai <ryota.arai@gmail.com>
2013-04-19 11:03:40 +0900

    ~ p.356

--- messages/app/models/message.rb
+++ messages/app/models/message.rb
@@ -1,3 +1,9 @@
 class Message < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :recipient, :text, :title
+
+  belongs_to :recipient, class_name: "User"
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :recipient, presence: true
 end
--- messages/db/schema.rb
+++ messages/db/schema.rb
@@ -11,11 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408234026) do
+ActiveRecord::Schema.define(:version => 20130411040818) do
 
   create_table "messages", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "title"
+    t.text     "text"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
